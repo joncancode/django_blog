c337a7dee1a29a47e658440f695df55ab4bccdf1
joncancode <jc.haines19@gmail.com>
2019-01-18 00:38:25 -0500

    add templates and routes

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,28 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Jon',
+        'title': 'blog post 1',
+        'content': 'content for first post',
+        'date_posted': 'Jan, 17, 2019'
+    },
+    {
+        'author': 'Other guy',
+        'title': 'blog post 2',
+        'content': 'content for second post',
+        'date_posted': 'Jan, 18, 2019'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>home!</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>about!</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
